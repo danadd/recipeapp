ed5f24487a33bbe08dbdd6919c8c3ce6dfa3c548
danadd <61254757+danadd@users.noreply.github.com>
2020-05-06 16:04:11 -0500

    Updates

---
+++ precache-manifest.7a0dc956f38d39b6b50c0df66efbd0f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc0ed1779fe542e196fe93b96f30c8fd",
+    "url": "/recipeapp/index.html"
+  },
+  {
+    "revision": "ad734dece9640615323e",
+    "url": "/recipeapp/static/css/main.cd388b5f.chunk.css"
+  },
+  {
+    "revision": "4d46637113a2350547bb",
+    "url": "/recipeapp/static/js/2.2c01a3e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipeapp/static/js/2.2c01a3e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad734dece9640615323e",
+    "url": "/recipeapp/static/js/main.a9795b02.chunk.js"
+  },
+  {
+    "revision": "c753f9156cdbd13a0369",
+    "url": "/recipeapp/static/js/runtime-main.540f4fd5.js"
+  }
+]);
